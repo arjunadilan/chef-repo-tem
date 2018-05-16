6a3188252929bc8b6533687c88620f23b7aff7fb
Dilan Arjuna <arjunadilan@outlook.com>
2018-05-16 12:00:52 +0800

    Ruby conditions and banner

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,11 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+if (node['platform_family'] == "amazon") or (node['platform_family'] == "rhel")
+    package = "httpd"
+elsif node['platform_family'] == "debian"
+    package = "apache2"
+else
+    package = "null"
+end
 package 'apache' do
-    package_name 'httpd'
+    package_name package
     action 'install'
 end
 service 'apache2' do 
-    service_name 'httpd'
+    service_name package
     action [:start, :enable]
 end
